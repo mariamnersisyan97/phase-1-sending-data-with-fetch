be7940d5d5fc5419d2bbd4dc0af1948fcb234896
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-03-12 17:14:28 -0500

    commit tree 4cce184317e397b38b79045023f0cecfa13173d2

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+// function submitData(firstName, email){
+    
+//     fetch('http://localhost:3000/users', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             Accept: 'application/json'
+//         },
+//         body:JSON.stringify({
+//             "name": firstName,
+//             "email": email
+//         })
+//     })
+//   };
+
+const body = document.querySelector('body');
+  function submitData(firstName, email){
+      const configObject ={
+        method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+             Accept: 'application/json'
+         },
+        body:JSON.stringify({
+           "name": firstName,
+           "email": email
+        })
+
+      };
+      return fetch('http://localhost:3000/users', configObject)
+      .then(res => res.json())
+      .then(body => newInfo(body.id))
+      .catch(errorReq)
+  }
+  function newInfo(newID) {
+      document.querySelector('body').innerHTML = newID
+  }
+
+  function errorReq(error){
+      document.body.innerHTML = error.message
+  }
